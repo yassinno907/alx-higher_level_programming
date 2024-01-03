13173a01bbb96ad672f271b1068beefb5bad01cf
yassinno907 <yassine.nopics95@gmail.com>
2024-01-03 03:25:33 -0800

    m

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[39:66], end = " ")
-print("with Python")
+str = " Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power  with very clear syntax"
+str = str[40:67] + str[-23:-18] + str[0:7]
+print(str)
